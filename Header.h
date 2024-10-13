ccdb38ff5155e48bd69d631ec285312978b2ee8a
abortish <fartbreather228@gmail.com>
2024-10-14 02:13:52 +0300

    первый коммит после создания репозитория. созданы пустые классы в студио, а также успешно выполнено их подключение между собой с помощью главного хедера

---
+++ Header.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include "human.h"
+#include "flat.h"
+#include "building.h"
+using namespace std;
+
---
+++ Source.cpp
@@ -0,0 +1,10 @@
+#include "Header.h"
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+
+
+	return 0;
+}
